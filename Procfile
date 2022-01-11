628af81ffa27eef828a9566e80177f59a29d83a1
Matin Rostami <tizmrmr@gmail.com>
2022-01-11 14:18:45 +0330

    added web flask

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 worker: python main.py
+web: gunicorn app:app
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from bot import bot
 import os
+
 from dotenv import load_dotenv
+
+from bot import bot
 
 if __name__ == "__main__":
     load_dotenv()
-    bot.run(os.getenv('TOKEN'))
-
+    bot.run(os.getenv("TOKEN"))
 
 # git pull heroku master
